ac3cc646acd6ec0da195b3245db14e19820a6b81
Singireddy Sumith reddy <sumithreddy.6080@gmail.com>
2025-05-25 16:03:10 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = ({ themeClasses }) => {
   return (
     <footer className={` fixed bottom-4 left-1/2 transform z-20 -translate-x-1/2 text-black text-md  ${themeClasses.textSecondary}`}>
-      Made with ❤️ by SingiReddy Sumith Reddy
+{/*       Made with ❤️ by SingiReddy Sumith Reddy */}
     </footer>
   );
 };
